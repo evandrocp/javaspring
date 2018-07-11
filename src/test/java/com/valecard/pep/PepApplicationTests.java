17bf0386dbeb4a5ef3eada32ae345d7b52999e2c
Evandro <evandro@ironsoft.com.br>
2018-07-11 14:45:40 -0300

    primeiro envio

---
+++ src/test/java/com/valecard/pep/PepApplicationTests.java
@@ -0,0 +1,16 @@
+package com.valecard.pep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PepApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
